29533f211882ffa59655a939dbd48b4a4ba0a274
Tony-20 <tonymatarrita20@gmail.com>
2022-11-12 00:47:48 -0600

    Views/Ended

--- Startup.cs
+++ Startup.cs
@@ -59,6 +59,8 @@ namespace WebNinnoFeliz
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            Rotativa.AspNetCore.RotativaConfiguration.Setup(env.WebRootPath, "../Rotativa");
         }
     }
 }
