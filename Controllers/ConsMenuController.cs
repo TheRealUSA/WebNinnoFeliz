b807cbdc10fc9c18bbcfeb89a5bf42f7202e7f28
Gree893 <grettel.rodriguez.munoz@est.una.ac.cr>
2022-11-14 16:16:30 -0600

    Vistas detalles y eliminar gerald

---
+++ Controllers/ConsMenuController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebNinnoFeliz.Data;
+using WebNinnoFeliz.Models;
+
+namespace WebNinnoFeliz.Controllers
+{
+    public class ConsMenuController : Controller
+    {
+        private readonly WebNinnoFelizContext _context;
+
+        public ConsMenuController(WebNinnoFelizContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ConsMenu
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Menus.ToListAsync());
+        }
+
+        // GET: ConsMenu/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus
+                .FirstOrDefaultAsync(m => m.IdNumeroMenu == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: ConsMenu/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ConsMenu/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdNumeroMenu,NombreMenu")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: ConsMenu/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: ConsMenu/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdNumeroMenu,NombreMenu")] Menu menu)
+        {
+            if (id != menu.IdNumeroMenu)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.IdNumeroMenu))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        
+
+        private bool MenuExists(int id)
+        {
+            return _context.Menus.Any(e => e.IdNumeroMenu == id);
+        }
+    }
+}
